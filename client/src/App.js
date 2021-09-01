8553b8e7d07a172c98f1370540dbfe0adf96e4c9
Shaelle11 <lakannanji@gmail.com>
2021-09-01 11:20:54 +0100

    chat engine changed to socket.io

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { StreamChat } from 'stream-chat';
-import { Chat } from 'stream-chat-react';
-import Cookies from 'universal-cookies';
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookies";
+import "./App.css";
 
+import { ChannelListContainer, ChannelContainer } from "./components";
 
-const apiKey = '92wwfwnduygk';
+const apiKey = "92wwfwnduygk";
 
-const client 
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div className='app__wrapper'>
-     <Chat>
-       <ChannelListContainer
-       
-       />
-       <ChannelContainer 
-       
-       />
-     </Chat>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team lights">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 };
